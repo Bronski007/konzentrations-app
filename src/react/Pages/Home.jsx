990bce52a9f54ef8f46130c6dcd41418a8691d6c
Bronski007 <152412743+Bronski007@users.noreply.github.com>
2025-12-05 13:46:26 +0100

    Add random fact feature

--- src/react/Pages/Home.jsx
+++ src/react/Pages/Home.jsx
@@ -1,19 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { Stack, Typography } from '@mui/material'
+import { Stack, Button, Typography, CircularProgress, Divider } from '@mui/material'
 
-const Home = () => (
-  <Stack
-    flex="1 1 auto"
-    justifyContent="center"
-    alignItems="center"
-  >
-    <Typography
-      variant="h4"
-    >
-      Welcome to the App
-    </Typography>
-  </Stack>
-)
+const Home = () => {
+  const [isWaiting, setIsWaiting] = useState(false)
+  const [fact, setFact] = useState('')
+
+  const serverCall = async () => {
+    setIsWaiting(true)
+    const r = await fetch('https://uselessfacts.jsph.pl/api/v2/facts/random?language=de')
+    const data = await r.json()
+    setFact(data.text)
+    setIsWaiting(false)
+  }
+
+  return (
+    <Stack flex="1 1 auto" justifyContent="center" alignItems="center" divider={<Divider orientation="horizontal" flexItem />} spacing={4} margin={2}>
+      <Typography variant="h3">
+        Zufällige Fakten
+      </Typography>
+      {
+        isWaiting
+          && <CircularProgress />
+      }
+      {
+        !isWaiting && fact
+          && <h3>{fact}</h3>
+      }
+      <Button variant="outlined" onClick={serverCall}>
+        Neuer Fakt
+      </Button>
+    </Stack>
+  )
+}
 
 export default Home
