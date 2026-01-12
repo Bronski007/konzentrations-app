1ffed6ddef870f51c8f4d27a9784246f39ba1030
Oliver Fritz <fritzolli05@gmail.com>
2026-01-12 11:02:31 +0100

    added basic background - demo

--- src/react/Pages/Home.jsx
+++ src/react/Pages/Home.jsx
@@ -1,18 +1,34 @@
 import React from 'react'
 
-import { Stack, Typography } from '@mui/material'
+import { Stack, Typography, Divider } from '@mui/material'
 
 const Home = () => (
   <Stack
     flex="1 1 auto"
-    justifyContent="center"
-    alignItems="center"
+    direction="column"
+    sx={{
+      width: '100%',
+      background: '#ccecfa'
+    }}
   >
     <Typography
       variant="h4"
+      sx={{
+        width: 600,
+        padding: '20px',
+        background: '#35afea',
+        fontWeight: 700
+      }}
     >
-      Test
+      Todo-planner
     </Typography>
+    <Divider
+      flexItem
+      sx={{
+        borderBottomWidth: 3,
+        background: '#039be5'
+      }}
+    />
   </Stack>
 )
 
