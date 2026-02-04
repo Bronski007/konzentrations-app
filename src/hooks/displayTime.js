c1282e459ad3d679524b2c901d61906c89fff5fc
MalteMaier <Malte.Maier@gmail.com>
2026-02-04 21:02:45 +0100

    displayTime changed

--- src/hooks/displayTime.js
+++ src/hooks/displayTime.js
@@ -1,11 +1,11 @@
 const displayTime = (total_seconds) => {
-    let seconds = total_seconds % 60
-    let minutes = ((total_seconds - seconds) / 60) % 60
-    let hours = ((total_seconds - seconds - minutes * 60)) / 3600
+  const seconds = total_seconds % 60
+  const minutes = ((total_seconds - seconds) / 60) % 60
+  const hours = ((total_seconds - seconds - minutes * 60)) / 3600
 
-    return (hours > 0? (hours < 10? "0" + hours : hours) + ":" : "")
-         + (minutes < 10? "0" + minutes : minutes) + ":"
-         + (seconds < 10? "0" + seconds : seconds)
+  return `${(hours > 0 ? `${hours < 10 ? `0${hours}` : hours}:` : '')
+         + (minutes < 10 ? `0${minutes}` : minutes)}:${
+    seconds < 10 ? `0${seconds}` : seconds}`
 }
 
 export default displayTime
