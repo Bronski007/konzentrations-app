de6b53ed055623184d1167578b050a190d73aad3
Bronski007 <152412743+Bronski007@users.noreply.github.com>
2026-02-05 20:14:55 +0100

    Add input validation for add-task-screen, fix linter errors in displayTime.js, make add-task-screen design consistent with start-task-screen design

--- src/react/Pages/Task.jsx
+++ src/react/Pages/Task.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 import { nanoid } from 'nanoid'
 import { useNavigate } from 'react-router-dom'
 
-import { Slider, Typography, Stack, TextField, Button, Box, Select, MenuItem, InputAdornment } from '@mui/material'
+import { Slider, Typography, Stack, TextField, Button, Box, Select, MenuItem, InputAdornment, Card, CardContent } from '@mui/material'
 import { DatePicker } from '@mui/x-date-pickers/DatePicker'
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
@@ -33,149 +33,171 @@ const Task = () => {
         width: '100%',
         display: 'flex',
         flexDirection: 'column',
-        overflow: 'hidden'
+        overflow: 'hidden',
+        background: '#fafcff'
       }}
     >
       <TopNavigationBar name="Task" />
       <Stack
         flex={1}
+        spacing={2}
         alignItems="center"
         sx={{
           width: '100%',
           overflowY: 'auto',
-          overflowX: 'hidden'
+          overflowX: 'hidden',
+          margin: '0 auto'
         }}
       >
-        <Stack
-          sx={{
-            width: '90%',
-            margin: '0 auto',
-            padding: 2,
-            paddingBottom: 4
-          }}
-          spacing={2}
-        >
-          <TextField
-            label="topic"
-            value={items.topic}
-            size="small"
-            required
-            onChange={(e) => {
-              const clonedItems = structuredClone(items)
-              clonedItems.topic = e.target.value
-              setItems(clonedItems)
-            }}
-          />
-
-          <TextField
-            label="title"
-            value={items.title}
-            size="small"
-            required
-            onChange={(e) => {
-              const clonedItems = structuredClone(items)
-              clonedItems.title = e.target.value
-              setItems(clonedItems)
-            }}
-          />
-
-          <TextField
-            label="approximated time"
-            size="small"
-            type="number"
-            value={items.approximatedTime.value}
-            required
-            onChange={(e) => {
-              const clonedItems = structuredClone(items)
-              clonedItems.approximatedTime.value = e.target.value
-              setItems(clonedItems)
-            }}
-            slotProps={{
-              input: {
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <Select
-                      variant="standard"
-                      value={items.approximatedTime.type}
-                      onChange={(e) => {
-                        const clonedItems = structuredClone(items)
-                        clonedItems.approximatedTime.type = e.target.value
-                        setItems(clonedItems)
-                      }}
-                      disableUnderline
-                    >
-                      <MenuItem value="h">h</MenuItem>
-                      <MenuItem value="min">min</MenuItem>
-                    </Select>
-                  </InputAdornment>
-                )
-              }
-            }}
-          />
-
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DatePicker
-              label="Deadline"
-              format="DD/MM/YYYY"
-              value={deadlineDate}
-              required
-              slotProps={{
-                textField: {
-                  size: 'small'
-                }
+        <Card sx={{ borderRadius: '2rem', width: '90%' }}>
+          <CardContent>
+            <Typography variant="h5" gutterBottom>create task</Typography>
+            <Stack
+              sx={{
+                width: '100%',
+                margin: '0 auto'
               }}
-              onChange={(newDeadline) => {
-                setDeadlineDate(newDeadline)
-              }}
-            />
-          </LocalizationProvider>
+              spacing={2}
+            >
+              <TextField
+                label="topic"
+                slotProps={{ input: { sx: { borderRadius: '1rem' } } }}
+                value={items.topic}
+                size="small"
+                onChange={(e) => {
+                  const clonedItems = structuredClone(items)
+                  clonedItems.topic = e.target.value
+                  setItems(clonedItems)
+                }}
+              />
 
-          <Box
-            spacing={1}
-            sx={{
-              border: 1,
-              borderColor: 'divider',
-              borderRadius: 1,
-              padding: 2,
-              flexDirection: 'column'
-            }}
-          >
-            <Typography>
-              Complexity:
-              {' '}
-              {items.complexity || 1}
-            </Typography>
+              <TextField
+                label="title"
+                slotProps={{ input: { sx: { borderRadius: '1rem' } } }}
+                value={items.title}
+                size="small"
+                onChange={(e) => {
+                  const clonedItems = structuredClone(items)
+                  clonedItems.title = e.target.value
+                  setItems(clonedItems)
+                }}
+              />
 
-            <Slider
-              value={items.complexity || 1}
-              min={1}
-              max={10}
-              step={1}
-              marks
-              valueLabelDisplay="auto"
-              onChange={(_, newValue) => {
-                const clonedItems = structuredClone(items)
-                clonedItems.complexity = newValue
-                setItems(clonedItems)
-              }}
-            />
-          </Box>
+              <TextField
+                label="approximated time"
+                size="small"
+                type="number"
+                value={items.approximatedTime.value}
+                onChange={(e) => {
+                  const clonedItems = structuredClone(items)
+                  clonedItems.approximatedTime.value = e.target.value
+                  setItems(clonedItems)
+                }}
+                slotProps={{
+                  input: {
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <Select
+                          variant="standard"
+                          value={items.approximatedTime.type}
+                          onChange={(e) => {
+                            const clonedItems = structuredClone(items)
+                            clonedItems.approximatedTime.type = e.target.value
+                            setItems(clonedItems)
+                          }}
+                          disableUnderline
+                          MenuProps={{
+                            PaperProps: {
+                              sx: {
+                                borderRadius: '1rem'
+                              }
+                            }
+                          }}
+                        >
+                          <MenuItem value="min">min</MenuItem>
+                          <MenuItem value="h">h</MenuItem>
+                        </Select>
+                      </InputAdornment>
+                    ),
+                    sx: { borderRadius: '1rem' }
+                  }
+                }}
+              />
 
-          <TextField
-            label="description"
-            value={items.description}
-            multiline
-            rows="5"
-            size="small"
-            required
-            onChange={(e) => {
-              const clonedItems = structuredClone(items)
-              clonedItems.description = e.target.value
-              setItems(clonedItems)
-            }}
-          />
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker
+                  label="deadline"
+                  format="DD/MM/YYYY"
+                  value={deadlineDate}
+                  slotProps={{
+                    textField: {
+                      size: 'small',
+                      InputProps: {
+                        sx: { borderRadius: '1rem' }
+                      }
+                    },
+                    popper: {
+                      sx: {
+                        '& .MuiPaper-root': {
+                          borderRadius: '1rem'
+                        }
+                      }
+                    }
+                  }}
+                  onChange={(newDeadline) => {
+                    setDeadlineDate(newDeadline)
+                  }}
+                />
+              </LocalizationProvider>
 
-        </Stack>
+              <Stack
+                spacing={1}
+                sx={{
+                  border: 1,
+                  borderColor: 'divider',
+                  borderRadius: '1rem',
+                  padding: 2,
+                  flexDirection: 'column'
+                }}
+              >
+                <Typography>
+                  complexity:
+                  {' '}
+                  {items.complexity || 1}
+                </Typography>
+
+                <Slider
+                  value={items.complexity || 1}
+                  min={1}
+                  max={10}
+                  step={1}
+                  marks
+                  valueLabelDisplay="auto"
+                  onChange={(_, newValue) => {
+                    const clonedItems = structuredClone(items)
+                    clonedItems.complexity = newValue
+                    setItems(clonedItems)
+                  }}
+                />
+              </Stack>
+
+              <TextField
+                label="description"
+                slotProps={{ input: { sx: { borderRadius: '1rem' } } }}
+                value={items.description}
+                multiline
+                rows="5"
+                size="small"
+                onChange={(e) => {
+                  const clonedItems = structuredClone(items)
+                  clonedItems.description = e.target.value
+                  setItems(clonedItems)
+                }}
+              />
+
+            </Stack>
+          </CardContent>
+        </Card>
         <Button
           variant="contained"
           onClick={() => {
@@ -191,10 +213,8 @@ const Task = () => {
             })
             navigate(-1)
           }}
-          sx={{
-            width: '60%',
-            marginBottom: '4%'
-          }}
+          sx={{ borderRadius: '2rem', width: '90%' }}
+          disabled={items.topic.trim() === '' || items.title.trim() === '' || items.description.trim() === '' || items.approximatedTime.value.trim() === '' || deadlineDate === null}
         >
           Create Task
         </Button>
